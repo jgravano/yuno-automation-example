c5baed6c0bdc7eb751999081a48e0fde5baa996e
juangravano <juangravano@gmail.com>
2022-12-28 20:32:47 -0300

    added customer helper

---
+++ src/helpers/customer_helper.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+class CustomerHelper:
+
+    def assert_current_date(self, response):
+        current_date = str(date.today())
+        assert current_date in response.json()['created_at'], f'The current date is {current_date}'
+
+   
